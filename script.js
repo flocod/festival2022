bb5be6c76d74c0cd606d5587be394111d673cfb1
flopeace <florian.tchomga@gmail.com>
2022-11-12 18:35:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  $("#input_name").on("input", () => {
+    let temp_name = $("#input_name").val();
+    $("#nom_invité").text(temp_name);
+  });
+
+  // teste si le Numero de téléphone est valide est valide
+  function validatePhone(txtPhone) {
+    var filter =
+      /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+    if (filter.test(txtPhone)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function generate() {
+    // var input_phone = $("#input_phone").val();
+    var input_name = $("#input_name").val();
+
+    if (input_name != "" || input_name.lenght>5) {
+
+      html2canvas(document.querySelector("#billet_invitation")).then(
+        (canvas) => {
+          $(canvas).addClass("image_invitation");
+
+          $("#container").prepend(canvas);
+        }
+      );
+
+      document.querySelector("#billet_invitation").style.display = "none";
+
+      setTimeout(function () {
+        canvas = $("canvas");
+
+        canvas.each(function () {
+          let temp = "";
+          temp = $(this).attr("class");
+          if (temp == "image_invitation") {
+            this.toBlob(function (blob) {
+              saveAs(blob, "billet_invitation_" + Date.now() + ".png");
+            });
+          }
+        });
+
+        window.location.replace("https://chat.whatsapp.com/KjhiCdJNLOjLHvPCGMDZmf");
+
+      }, 1000);
+       
+
+    } else {
+      alert("veuillez remplir le formulaire !");
+    }
+  }
+
+  $(document).ready(function () {
+    $("body").on("click", "#generate", function () {
+      generate();
+    });
+  });
+});
